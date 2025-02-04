2f5940ae182e6b3cfa30093820db13ef0a5b6dac
jordan <jordandimst@gmail.com>
2025-02-03 22:37:23 -0300

    solution remove duplicates

---
+++ src/main/java/org/com/challenges/easy/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package org.com.challenges.easy;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        int prenchePosicao = 1;
+        int tamanhoArray = nums.length;
+        for(int i= 1; i < tamanhoArray; i++){
+            if(nums[prenchePosicao - 1] != nums[i]){
+                nums[prenchePosicao++] = nums[i];
+            }
+        }
+        return prenchePosicao;
+    }
+
+    public static void main(String[] args){
+        int[] array = {0,0,1,1,1,2,2,3,3,4};
+        removeDuplicates(array);
+    }
+}
