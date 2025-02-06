f62503d6462cbea0054082aebafdbe7318b1095e
jordan <jordandimst@gmail.com>
2025-02-06 18:03:49 -0300

    solution remove element

---
+++ src/main/java/org/com/challenges/easy/RemoveElement.java
@@ -0,0 +1,21 @@
+package org.com.challenges.easy;
+
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int tamanhoArray = nums.length;
+        int prox = 0;
+
+        for(int i = 0; i < tamanhoArray; i++){
+            if(nums[i] != val){
+                nums[prox++] = nums[i];
+                continue;
+            }
+        }
+        return prox;
+    }
+
+    public static void main(String[] args){
+        int[] array = {0,1,2,2,3,0,4,2};
+        removeElement(array, 2);
+    }
+}
