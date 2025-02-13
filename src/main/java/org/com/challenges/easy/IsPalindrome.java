bf8da89ce1293473a3cc12204987d03acb868e4b
JordanTrindade <jordandimst@gmail.com>
2025-02-12 21:53:33 -0300

    solution IsPalindrome.java.java

---
+++ src/main/java/org/com/challenges/easy/IsPalindrome.java
@@ -0,0 +1,30 @@
+package org.com.challenges.easy;
+
+public class IsPalindrome {
+    public static boolean isPalindrome(int x) {
+        String numberString = String.valueOf(x);
+
+        for(int i = 0, j = numberString.length() - 1; i < j; i++, j--){
+            if(numberString.charAt(i) != numberString.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+        /*
+        *
+        * solucao mais lenta
+        *
+        *
+        String reversed = new StringBuilder(numberString).reverse().toString();
+        if(numberString.equalsIgnoreCase(reversed) ){
+            return  true;
+        }
+        return false;
+        *
+        */
+    }
+
+    public static void main(String[] args) {
+        System.out.println(IsPalindrome.isPalindrome(10));
+    }
+}
