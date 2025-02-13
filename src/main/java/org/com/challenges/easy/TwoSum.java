78806f7946063f8f01c1bb502119ade9d251a22e
JordanTrindade <jordandimst@gmail.com>
2025-02-12 21:53:24 -0300

    solution TwoSum.java

--- src/main/java/org/com/challenges/easy/TwoSum.java
+++ src/main/java/org/com/challenges/easy/TwoSum.java
@@ -1,4 +1,17 @@
 package org.com.challenges.easy;
 
+import java.util.*;
+
 public class TwoSum {
+    public int[] twoSum(int[] num, int target){
+        Map<Integer, Integer> nuns = new HashMap<>(); //map onde key = num do array, valor = posicao
+        for(int i = 0; i <= num.length; i++){
+            int complemento = target - num[i];
+            if(nuns.containsKey(complemento)){
+                return new int[]{nuns.get(complemento), i} ;
+            }
+            nuns.put(num[i], i);
+        }
+        return new int[]{};
+    }
 }
