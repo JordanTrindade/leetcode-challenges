65181f8541fd48793c583ca815e0f3d72408634f
JordanTrindade <jordandimst@gmail.com>
2025-02-13 11:32:12 -0300

    solution RomanToInt.java

---
+++ src/main/java/org/com/challenges/easy/RomanToInt.java
@@ -0,0 +1,42 @@
+package org.com.challenges.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInt {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> symbolToValue = new HashMap<>();
+        int soma = 0;
+        int i = 0;
+        int tamanhoString = s.length();
+
+        symbolToValue.put('I',1);
+        symbolToValue.put('V',5);
+        symbolToValue.put('X',10);
+        symbolToValue.put('L',50);
+        symbolToValue.put('C',100);
+        symbolToValue.put('D',500);
+        symbolToValue.put('M',1000);
+
+        while(i < tamanhoString){
+            int valorAtual = symbolToValue.get(s.charAt(i));
+            if(i + 1 < tamanhoString){
+                int valorProx = symbolToValue.get(s.charAt(i+1));
+
+                if(valorAtual < valorProx){
+                    soma += valorProx - valorAtual;
+                    i += 2;
+                    continue;
+                }
+            }
+            soma += valorAtual;
+            i++;
+        }
+        return soma;
+    }
+
+    public static void main(String[] args){
+        System.out.println("soma : " + RomanToInt.romanToInt("MCMXCIV"));
+
+    }
+}
