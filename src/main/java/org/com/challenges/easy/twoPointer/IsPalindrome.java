bc144af3d46b726b248c6e5b7a01e87cc93dcf21
jordan <jordandimst@gmail.com>
2025-02-14 22:31:22 -0300

    solution IsPalindrome

---
+++ src/main/java/org/com/challenges/easy/twoPointer/IsPalindrome.java
@@ -0,0 +1,19 @@
+package org.com.challenges.easy.twoPointer;
+
+public class IsPalindrome {
+    public static boolean isPalindrome(String s) {
+        String regex = "[^a-zA-Z0-9]+";
+        String stringReplaced = s.replaceAll(regex,"").toLowerCase();
+        System.out.println(stringReplaced);
+        for(int i = 0, j = stringReplaced.length()-1; i < j; i++, j--){
+            if(stringReplaced.charAt(i) != stringReplaced.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
