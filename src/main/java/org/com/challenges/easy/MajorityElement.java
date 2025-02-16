4564608304c82ff20682af289f8ef2c29f5595d9
jordan <jordandimst@gmail.com>
2025-02-15 21:56:59 -0300

    solution MajorityElement

--- src/main/java/org/com/challenges/easy/MajorityElement.java
+++ src/main/java/org/com/challenges/easy/MajorityElement.java
@@ -1,4 +1,25 @@
 package org.com.challenges.easy;
 
+import java.util.HashMap;
+
 public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> majority = new HashMap<>();
+        Integer maiorValor = 0;
+        Integer maiorChave = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (majority.containsKey(nums[i])) {
+                majority.put(nums[i], majority.get(nums[i]) + 1);
+            } else {
+                majority.put(nums[i], 1);
+            }
+        }
+        for(var maps : majority.entrySet()){
+            if(maps.getValue() > maiorValor){
+                maiorValor = maps.getValue();
+                maiorChave = maps.getKey();
+            }
+        }
+        return maiorChave;
+    }
 }
