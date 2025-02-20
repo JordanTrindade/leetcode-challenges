ecd9e57de0fba411a83a27664b41071f5e5903bd
jordan <jordandimst@gmail.com>
2025-02-19 22:09:12 -0300

    solution MaxProfit

---
+++ src/main/java/org/com/challenges/easy/arrays/MaxProfit.java
@@ -0,0 +1,16 @@
+package org.com.challenges.easy.arrays;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > profit) {
+                profit = price - minPrice;
+            }
+        }
+        return profit;
+    }
+}
