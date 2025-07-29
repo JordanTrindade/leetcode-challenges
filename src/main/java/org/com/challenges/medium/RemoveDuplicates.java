41d3b013a631033b09daf8c95374b7337e3daf5d
jordan <jordandimst@ gmail.com>
2025-07-29 16:21:51 -0300

    remove duplicatas solution

---
+++ src/main/java/org/com/challenges/medium/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package org.com.challenges.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        int count= 0;
+        int writeIndex= 2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i] != nums[writeIndex-2]){
+                nums[writeIndex] = nums[i];
+                writeIndex++;
+            }
+        }
+        return writeIndex;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,1,1,2,2,3};
+        removeDuplicates(nums);
+    }
+}
